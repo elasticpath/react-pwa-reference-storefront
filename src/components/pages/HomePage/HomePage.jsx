60da7cf3b2961c26910d825ddfba0fab8af9ea46
Shaun Maharaj <Shaun.Maharaj@elasticpath.com>
2018-06-28 00:21:39 -0400

    - Fixes for footer and Homepage

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -25,27 +25,28 @@ import AppFooterMain from '../../ui/appfooter/appfooter.main.jsx';
 var Config = require('Config')
 // Then use in render: <span>{Config.skuImagesS3Url}</span>
 
+import home_espot_main from '../../../style/images/Hero-Image-v2.jpg';
+import home_espot1 from '../../../style/images/VESTRI_CR-550_Banner.jpeg';
+import home_espot2 from '../../../style/images/VESTRI_M-CLASS_3_Banner.jpeg';
+import home_espot3 from '../../../style/images/VESTRI_X-CLASS_EL2_Banner.jpeg';
+
 class HomePage extends React.Component {
     render() {
         return (
             <div className="viewport ui-container home-ui-container" data-region="viewPortRegion" style={{ display: 'block' }}>
                 <div>
                     <AppHeaderMain />
-                    <div className="app-main home-contant-container" data-region="appMain" style={{ display: 'block' }}>
+                    <div className="home-contant-container" style={{ display: 'block' }}>
                         <div>
-                            <div data-region="homeMainContentRegion" className="container" style={{ display: 'block' }}>
-                                <div className="home-container">
-                                    <div className="field-content">
-                                        <h2>Commerce software that powers the next generation of digital experience</h2>
-                                    </div>
-                                    <div data-region="EPWidgetOne"></div>
-                                </div>
+                            <div data-region="homeMainContentRegion" className="home-espot-container container" style={{ display: 'block' }}>
+                                <img className="home-espot-1" src={home_espot_main} />
                             </div>
-                            <div className="sub-espot-container container">
-                                <div className="home-espot-1">
-                                </div>
-                                <div className="home-espot-2">
-                                </div>
+                            <div className="home-sub-espot-container1 container">
+                                <img className="home-espot-2" src={home_espot1} />
+                                <img className="home-espot-3" src={home_espot2} />
+                            </div>
+                            <div data-region="homeMainContentRegion" className="home-sub-espot-container2 container" style={{ display: 'block' }}>
+                                <img className="home-espot-4" src={home_espot3} />
                             </div>
                         </div>
                     </div>
