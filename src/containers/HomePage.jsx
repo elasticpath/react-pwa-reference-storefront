fb0be3aba0d95504544397f9dfb320885d6ded5d
Dusan Radovanovic <41649443+dusanradovanovic@users.noreply.github.com>
2018-09-14 11:55:49 -0700

    Less refactor of home page (#9)
    * Moved Montserrat font to less file
    
    * Refactored home page

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -27,6 +27,8 @@ import homeEspot2 from '../images/site-images/brake-icon.jpg';
 import homeEspot3 from '../images/site-images/charging-icon.png';
 import homeEspot4 from '../images/site-images/warranty-graphic.jpg';
 
+import './HomePage.less';
+
 const Config = require('Config');
 
 const homeEspotMainFileName = 'homepage-banner.jpg';
@@ -36,66 +38,60 @@ const homeEspot4FileName = 'warranty-graphic.jpg';
 
 function HomePage() {
   return (
-    <div className="viewport ui-container home-ui-container" data-region="viewPortRegion" style={{ display: 'block' }}>
-      <div>
-        <div className="home-contant-container" style={{ display: 'block' }}>
-          <div>
-            <div data-region="homeMainContentRegion" className="home-espot-container container" style={{ display: 'block' }}>
-              <img alt="home-espot-1" className="home-espot-1" src={Config.siteImagesUrl.replace('%fileName%', homeEspotMainFileName)} onError={(e) => { e.target.src = homeEspotMain; }} />
-            </div>
-            <div className="home-sub-espot-container1 container">
-              <div className="home-sub-espot-container1-sub">
-                <span className="home-sub-espot-container1-first-line">
-                  {intl.get('home-sub-espot-container1-first-line')}
-                </span>
-                <span className="home-sub-espot-container1-second-line">
-                  {intl.get('home-sub-espot-container1-second-line')}
-                </span>
-                <button className="home-sub-espot-container1-button" data-toggle="collapse" data-target=".navbar-collapse" type="submit">
-                  {intl.get('home-sub-espot-container1-button')}
-                </button>
-              </div>
-            </div>
-            <div className="home-sub-espot-container2 container">
-              <div className="home-sub-espot-container2-sub">
-                <img alt="home-espot-2" className="home-espot-2" src={Config.siteImagesUrl.replace('%fileName%', homeEspot2FileName)} onError={(e) => { e.target.src = homeEspot2; }} />
-                <div className="home-sub-espot-container2-text">
-                  <span className="home-sub-espot-container2-first-line">
-                    {intl.get('home-sub-espot-container2-first-line')}
-                  </span>
-                  <span className="home-sub-espot-container2-second-line">
-                    {intl.get('home-sub-espot-container2-second-line')}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="home-sub-espot-container3 container">
-              <div className="home-sub-espot-container3-sub">
-                <img alt="home-espot-3" className="home-espot-3-mobile" src={Config.siteImagesUrl.replace('%fileName%', homeEspot3FileName)} onError={(e) => { e.target.src = homeEspot3; }} />
-                <div className="home-sub-espot-container3-text">
-                  <span className="home-sub-espot-container3-first-line">
-                    {intl.get('home-sub-espot-container3-first-line')}
-                  </span>
-                  <span className="home-sub-espot-container3-second-line">
-                    {intl.get('home-sub-espot-container3-second-line')}
-                  </span>
-                </div>
-                <img alt="home-espot-3" className="home-espot-3" src={Config.siteImagesUrl.replace('%fileName%', homeEspot3FileName)} onError={(e) => { e.target.src = homeEspot3; }} />
-              </div>
-            </div>
-            <div className="home-sub-espot-container4 container">
-              <div className="home-sub-espot-container4-sub">
-                <img alt="home-espot-4" className="home-espot-4" src={Config.siteImagesUrl.replace('%fileName%', homeEspot4FileName)} onError={(e) => { e.target.src = homeEspot4; }} />
-                <div className="home-sub-espot-container4-text">
-                  <span className="home-sub-espot-container4-first-line">
-                    {intl.get('home-sub-espot-container4-first-line')}
-                  </span>
-                  <span className="home-sub-espot-container4-second-line">
-                    {intl.get('home-sub-espot-container4-second-line')}
-                  </span>
-                </div>
-              </div>
-            </div>
+    <div className="home-page-component" data-region="viewPortRegion">
+      <div className="section section-1 container" data-region="homeMainContentRegion">
+        <img className="cover" alt="home-espot-1" src={Config.siteImagesUrl.replace('%fileName%', homeEspotMainFileName)} onError={(e) => { e.target.src = homeEspotMain; }} />
+      </div>
+      <div className="section section-2 container">
+        <div className="sub-section">
+          <span className="line line-1">
+            {intl.get('home-sub-espot-container1-first-line')}
+          </span>
+          <span className="line line-2">
+            {intl.get('home-sub-espot-container1-second-line')}
+          </span>
+          <button className="btn-accessories" data-toggle="collapse" data-target=".navbar-collapse" type="submit">
+            {intl.get('home-sub-espot-container1-button')}
+          </button>
+        </div>
+      </div>
+      <div className="section section-3 container">
+        <div className="sub-section">
+          <img className="small-image" alt="home-espot-2" src={Config.siteImagesUrl.replace('%fileName%', homeEspot2FileName)} onError={(e) => { e.target.src = homeEspot2; }} />
+          <div className="text-block">
+            <span className="line line-1">
+              {intl.get('home-sub-espot-container2-first-line')}
+            </span>
+            <span className="line line-2">
+              {intl.get('home-sub-espot-container2-second-line')}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="section section-4 container">
+        <div className="sub-section">
+          <img className="small-image image-1" alt="home-espot-3" src={Config.siteImagesUrl.replace('%fileName%', homeEspot3FileName)} onError={(e) => { e.target.src = homeEspot3; }} />
+          <div className="text-block">
+            <span className="line line-1">
+              {intl.get('home-sub-espot-container3-first-line')}
+            </span>
+            <span className="line line-2">
+              {intl.get('home-sub-espot-container3-second-line')}
+            </span>
+          </div>
+          <img className="small-image image-2" alt="home-espot-3" src={Config.siteImagesUrl.replace('%fileName%', homeEspot3FileName)} onError={(e) => { e.target.src = homeEspot3; }} />
+        </div>
+      </div>
+      <div className="section section-5 container">
+        <div className="sub-section">
+          <img className="small-image" alt="home-espot-4" src={Config.siteImagesUrl.replace('%fileName%', homeEspot4FileName)} onError={(e) => { e.target.src = homeEspot4; }} />
+          <div className="text-block">
+            <span className="line line-1">
+              {intl.get('home-sub-espot-container4-first-line')}
+            </span>
+            <span className="line line-2">
+              {intl.get('home-sub-espot-container4-second-line')}
+            </span>
           </div>
         </div>
       </div>
