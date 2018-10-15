a15792f1f88136949a2408dcf14545aa818f1933
Michael Szyszko <michael.szyszko@elasticpath.com>
2018-10-15 15:05:08 -0400

    promotions componentized

--- src/components/add.promotion.container.jsx
+++ src/components/add.promotion.container.jsx
@@ -19,35 +19,155 @@
  *
  */
 
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import intl from 'react-intl-universal';
-// import { login } from '../utils/AuthService';
-// import cortexFetch from '../utils/Cortex';
-// import './checkout.summarylist.less';
+import React from 'react';
+import PropTypes from 'prop-types';
+import intl from 'react-intl-universal';
+import { login } from '../utils/AuthService';
+import cortexFetch from '../utils/Cortex';
+import './add.promotion.container.less';
 
-// const Config = require('Config');
+const Config = require('Config');
 
-// class CheckoutSummaryList extends React.Component {
-//   static propTypes = {
-//     data: PropTypes.objectOf(PropTypes.any).isRequired,
-//     onSubmittedPromotion: PropTypes.func,
-//     renderAddPromotion: PropTypes.bool,
-//   }
-//   static defaultProps = {
-//     renderAddPromotion: false,
-//     onSubmittedPromotion: null,
-//   }
+class AddPromotionContainer extends React.Component {
+  static propTypes = {
+    // data: PropTypes.objectOf(PropTypes.any).isRequired,
+    onSubmittedPromotion: PropTypes.func,
+    renderAddPromotion: PropTypes.bool,
+  }
 
-//   constructor() {
-//     super();
-//     this.state = {
-//       isPromotionFormOpen: false,
-//       failedPromotion: false,
-//       promotionCode: '',
-//       couponFormLink: 'default',
-//     };
-//     this.setPromotionCode = this.setPromotionCode.bind(this);
-//     this.submitPromotionCode = this.submitPromotionCode.bind(this);
-//   }
-// }
+  static defaultProps = {
+    renderAddPromotion: false,
+    onSubmittedPromotion: null,
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      isPromotionFormOpen: false,
+      failedPromotion: false,
+      promotionCode: '',
+      couponFormLink: 'default',
+    };
+    this.setPromotionCode = this.setPromotionCode.bind(this);
+    this.submitPromotionCode = this.submitPromotionCode.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCouponForm();
+  }
+
+  setPromotionCode(event) {
+    this.setState({ promotionCode: event.target.value });
+  }
+
+  openPromotionForm() {
+    this.setState({
+      isPromotionFormOpen: true,
+    });
+  }
+
+  closePromotionForm() {
+    this.setState({
+      isPromotionFormOpen: false,
+      failedPromotion: false,
+    });
+  }
+
+  fetchCouponForm() {
+    login().then(() => {
+      cortexFetch('/?zoom=defaultcart:order:couponinfo:couponform', {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
+        },
+      })
+        .then(res => res.json())
+        .then((res) => {
+          const couponFormUri = res._defaultcart[0]._order[0]._couponinfo[0]._couponform[0].links.find(
+            link => link.rel === 'applycouponaction',
+          ).uri;
+          this.setState({
+            couponFormLink: couponFormUri,
+          });
+        });
+    });
+  }
+
+  submitPromotionCode(event) {
+    event.preventDefault();
+    const { onSubmittedPromotion } = this.props;
+    const { couponFormLink, promotionCode } = this.state;
+    login().then(() => {
+      cortexFetch(couponFormLink, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
+        },
+        body: JSON.stringify({
+          code: promotionCode,
+        }),
+      }).then((res) => {
+        if (res.status === 409) {
+          this.setState({ failedPromotion: true });
+        } else if (res.status === 201 || res.status === 200 || res.status === 204) {
+          this.setState({ failedPromotion: false }, () => {
+            this.closePromotionForm();
+            onSubmittedPromotion();
+          });
+        }
+      }).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error.message);
+      });
+    });
+  }
+
+  render() {
+    const {
+      isPromotionFormOpen,
+      failedPromotion,
+    } = this.state;
+    const { renderAddPromotion } = this.props;
+    if (renderAddPromotion && isPromotionFormOpen) {
+      return (
+        <div className="add-promotion-container">
+          <h2>
+            {intl.get('add-promotion-title')}
+          </h2>
+          <form className="form-horizontal" onSubmit={this.submitPromotionCode}>
+            <div>
+              <div>
+                {failedPromotion ? intl.get('promotion-invalid') : ''}
+              </div>
+              <div className="form-group">
+                <div>
+                  <input id="promotion_form_code" name="PromotionCode" className="form-control" type="text" defaultValue="" onChange={this.setPromotionCode} />
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <button className="btn btn-add-promotion" type="submit">
+                {intl.get('apply-promotion')}
+              </button>
+              <button className="btn btn-add-promotion" type="button" onClick={() => { this.closePromotionForm(); }}>
+                {intl.get('cancel')}
+              </button>
+            </div>
+          </form>
+        </div>
+      );
+    }
+    if (renderAddPromotion) {
+      return (
+        <div className="add-promotion-container">
+          <button className="btn btn-add-promotion" type="button" id="open_promotion_form_button" onClick={() => { this.openPromotionForm(); }}>
+            {intl.get('add-promotion')}
+          </button>
+        </div>
+      );
+    }
+    return null;
+  }
+}
+export default AddPromotionContainer;
