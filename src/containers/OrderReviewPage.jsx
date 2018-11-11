48f497c983f5226dc48bbe3867cd3d9f082d3125
Shaun Maharaj <Shaun.Maharaj@elasticpath.com>
2018-11-11 16:05:37 -0500

    Move deliveries constant

--- src/containers/OrderReviewPage.jsx
+++ src/containers/OrderReviewPage.jsx
@@ -148,8 +148,8 @@ class OrderReviewPage extends React.Component {
 
   trackTransactionAnalytics() {
     const { orderData } = this.state;
-    const deliveries = (orderData._order[0]._deliveries) ? orderData._order[0]._deliveries[0]._element[0]._shippingoptioninfo[0]._shippingoption[0].cost[0].display : '';
     if (isAnalyticsConfigured()) {
+      const deliveries = (orderData._order[0]._deliveries) ? orderData._order[0]._deliveries[0]._element[0]._shippingoptioninfo[0]._shippingoption[0].cost[0].display : '';
       trackAddTransactionAnalytics(orderData.self.uri.split(`/carts/${Config.cortexApi.scope}/`)[1], orderData._order[0]._total[0].cost[0].amount, deliveries, orderData._order[0]._tax[0].total.display);
       orderData._lineitems[0]._element.map((product) => {
         const categoryTag = (product._item[0]._definition[0].details) ? (product._item[0]._definition[0].details.find(detail => detail['display-name'] === 'Tag')) : '';
