46892712fafba908fc9cedf9032c2e15ffada5fe
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2018-11-12 17:54:38 -0800

    Refactored generateFormBody

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -23,38 +23,30 @@ import cortexFetch from './Cortex';
 
 const Config = require('Config');
 
-let userFormBody = [];
-let userFormBodyString = '';
 let newaccountform = '';
 
 function generateFormBody(userDetails) {
-  Object.keys(userDetails).forEach((encodedKey) => {
-    const encodedValue = userDetails[encodedKey];
-    userFormBody.push(`${encodedKey}=${encodedValue}`);
-  });
-  userFormBodyString = userFormBody.join('&');
+  return Object.keys(userDetails)
+    .map(key => `${key}=${userDetails[key]}`)
+    .join('&');
 }
 
 export function login() {
   return new Promise(((resolve, reject) => {
     if (localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`) === null) {
-      userFormBodyString = '';
-      userFormBody = [];
-      const publicUserDetails = {
-        username: '',
-        password: '',
-        grant_type: 'password',
-        role: 'PUBLIC',
-        scope: Config.cortexApi.scope,
-      };
-      generateFormBody(publicUserDetails);
 
       cortexFetch('/oauth2/tokens', {
         method: 'post',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
         },
-        body: userFormBodyString,
+        body: generateFormBody({
+          username: '',
+          password: '',
+          grant_type: 'password',
+          role: 'PUBLIC',
+          scope: Config.cortexApi.scope,
+        }),
       }).then((res) => {
         if (res.status === 504 || res.status === 503) {
           reject(res);
@@ -65,7 +57,7 @@ export function login() {
           localStorage.setItem(`${Config.cortexApi.scope}_oAuthRole`, res.role);
           localStorage.setItem(`${Config.cortexApi.scope}_oAuthScope`, res.scope);
           localStorage.setItem(`${Config.cortexApi.scope}_oAuthToken`, `Bearer ${res.access_token}`);
-          localStorage.setItem(`${Config.cortexApi.scope}_oAuthUserName`, publicUserDetails.username);
+          localStorage.setItem(`${Config.cortexApi.scope}_oAuthUserName`, '');
           resolve(res);
         })
         .catch((error) => {
@@ -74,33 +66,63 @@ export function login() {
           reject(error);
         });
     } else {
-      resolve(userFormBodyString);
+      resolve();
     }
   }));
+}
+
+export function login2() {
+  if (localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`)) {
+    return Promise.resolve(200);
+  }
+
+  return cortexFetch('/oauth2/tokens', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+    body: generateFormBody({
+      grant_type: 'password',
+      role: 'PUBLIC',
+      scope: Config.cortexApi.scope,
+    }),
+  })
+    .then((res) => {
+      if (res.status === 504 || res.status === 503) {
+        throw new Error();
+      }
+      return res;
+    })
+    .then(res => res.json())
+    .then((body) => {
+      localStorage.setItem(`${Config.cortexApi.scope}_oAuthRole`, body.role);
+      localStorage.setItem(`${Config.cortexApi.scope}_oAuthScope`, body.scope);
+      localStorage.setItem(`${Config.cortexApi.scope}_oAuthToken`, `Bearer ${body.access_token}`);
+      localStorage.setItem(`${Config.cortexApi.scope}_oAuthUserName`, '');
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error(error.message);
+      throw error;
+    });
 }
 
 export function loginRegistered(username, password) {
   return new Promise(((resolve, reject) => {
     if (localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`) != null) {
-      userFormBodyString = '';
-      userFormBody = [];
-      const registeredUserDetails = {
-        username,
-        password,
-        grant_type: 'password',
-        role: 'REGISTERED',
-        scope: Config.cortexApi.scope,
-      };
-
-      generateFormBody(registeredUserDetails);
-
       cortexFetch('/oauth2/tokens', {
         method: 'post',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
           Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
         },
-        body: userFormBodyString,
+        body: generateFormBody({
+          username,
+          password,
+          grant_type: 'password',
+          role: 'REGISTERED',
+          scope: Config.cortexApi.scope,
+        }),
       }).then((res) => {
         if (res.status === 401) {
           resolve(401);
@@ -115,7 +137,7 @@ export function loginRegistered(username, password) {
         localStorage.setItem(`${Config.cortexApi.scope}_oAuthRole`, res.role);
         localStorage.setItem(`${Config.cortexApi.scope}_oAuthScope`, res.scope);
         localStorage.setItem(`${Config.cortexApi.scope}_oAuthToken`, `Bearer ${res.access_token}`);
-        localStorage.setItem(`${Config.cortexApi.scope}_oAuthUserName`, registeredUserDetails.username);
+        localStorage.setItem(`${Config.cortexApi.scope}_oAuthUserName`, username);
         resolve(200);
       }).catch((error) => {
         // eslint-disable-next-line no-console
@@ -123,10 +145,49 @@ export function loginRegistered(username, password) {
         reject(error);
       });
     } else {
-      resolve(userFormBodyString);
+      resolve(200);
     }
   }));
 }
+
+export function loginRegistered2(username, password) {
+  return cortexFetch('/oauth2/tokens', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+    body: generateFormBody({
+      username,
+      password,
+      grant_type: 'password',
+      role: 'REGISTERED',
+      scope: Config.cortexApi.scope,
+    }),
+  })
+    .then(res => Promise.all([res, res.status === 200 ? res.json() : {}]))
+    .then(([res, body]) => {
+      if (res.status === 200) {
+        localStorage.setItem(`${Config.cortexApi.scope}_oAuthRole`, body.role);
+        localStorage.setItem(`${Config.cortexApi.scope}_oAuthScope`, body.scope);
+        localStorage.setItem(`${Config.cortexApi.scope}_oAuthToken`, `Bearer ${body.access_token}`);
+        localStorage.setItem(`${Config.cortexApi.scope}_oAuthUserName`, username);
+      }
+
+      return res.status;
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error(error.message);
+      throw error;
+    });
+}
+
+// export function login() {
+//   if (localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`)) {
+//     return Promise.resolve(200);
+//   }
+
+//   return loginRegistered('', '');
+// }
 
 export function logout() {
   return new Promise(((resolve, reject) => {
@@ -144,6 +205,15 @@ export function logout() {
       reject(error);
     });
   }));
+}
+
+export function logout2() {
+  localStorage.removeItem(`${Config.cortexApi.scope}_oAuthRole`);
+  localStorage.removeItem(`${Config.cortexApi.scope}_oAuthScope`);
+  localStorage.removeItem(`${Config.cortexApi.scope}_oAuthToken`);
+  localStorage.removeItem(`${Config.cortexApi.scope}_oAuthUserName`);
+
+  return Promise.resolve();
 }
 
 export function getRegistrationForm() {
