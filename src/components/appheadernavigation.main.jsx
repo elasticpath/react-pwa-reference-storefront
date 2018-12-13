ecebddf4f7f60691f2583e3faee5cafcc15eba58
Alvin <alvin.chan@elasticpath.com>
2018-12-13 10:08:33 -0500

    Able to show submenu catalog

--- src/components/appheadernavigation.main.jsx
+++ src/components/appheadernavigation.main.jsx
@@ -141,161 +141,70 @@ class AppHeaderNavigationMain extends React.Component {
       return (
         subcategoryChild._child
           ? (
-            <li className="dropdown-submenu" key={subcategoryChild.name} data-name={subcategoryChild['display-name']} data-el-container="category-nav-item-container">
-              <label className="dropdown-item" id={`${isMobileView ? 'mobile_' : ''}navbarDropdown_${subcategoryChild.name}`}>
+            <li className="nav-item dropdown">
+              <a className="dropdown-item dropdown-toggle" href="https://bootstrapthemes.co" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 {subcategoryChild['display-name']}
-              </label>
-              <ul className="dropdown-menu sub-category-dropdown-menu" role="menu" aria-label={`navbarDropdown_${subcategoryChild.name}`}>
-                {subcategoryChild._child.map(subcategory => this.renderSubCategories([subcategory]))}
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                {this.renderSubCategories(subcategoryChild._child)}
               </ul>
             </li>
           )
           : (
-            <li className="dropdown-item" key={subcategoryChild.name} data-name={subcategoryChild['display-name']} data-toggle="collapse" data-target=".navbar-collapse">
-              <Link className="nav-link dropdown-item" to={`/category/${subcategoryChild.name}`}>
-                <div
-                  data-toggle="collapse"
-                  data-target={isMobileView ? '.collapsable-container' : ''}
-                >
+            <li>
+                <a className="dropdown-item" href="#">
                   {subcategoryChild['display-name']}
-                </div>
-              </Link>
+                </a>
             </li>
           )
-
       );
     });
   }
 
+  // TODO:
   renderCategories() {
     const { navigations } = this.state;
     const { isMobileView } = this.props;
-    console.log(navigations);
-    if (navigations) {
-      return navigations.map((category) => {
-        if (category._child) {
-          return (
-            <li className="nav-item dropdown" key={category.name} data-name={category['display-name']} data-toggle="collapse" data-target=".navbar-collapse">
-              <label className="nav-link dropdown-toggle" id={`${isMobileView ? 'mobile_' : ''}navbarDropdown_${category.name}`} role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {category['display-name']}
-              </label>
-              <ul className="dropdown-menu sub-category-dropdown-menu" role="menu" aria-label={`navbarDropdown_${category.name}`}>
-                {this.renderSubCategories(category._child)}
-              </ul>
-            </li>
-          );
-        }
+
+    return (navigations.map((category) => {
+      if (category._child) {
         return (
-          <li className="nav-item" key={category.name} data-name={category['display-name']} data-toggle="collapse" data-target=".navbar-collapse">
-            <Link className="nav-link" to={`/category/${category.name}`}>
-              <div
-                data-toggle="collapse"
-                data-target={isMobileView ? '.collapsable-container' : ''}
-              >
-                {category['display-name']}
-              </div>
-            </Link>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="https://bootstrapthemes.co" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              {category['display-name']}
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              {this.renderSubCategories(category._child)}
+            </ul>
           </li>
         );
-      });
-    }
-    return null;
+      } else {
+        return (
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="https://bootstrapthemes.co" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              {category['display-name']}
+            </a>
+          </li>
+        );
+      }
+    }));
   }
 
   render() {
     const { navigations } = this.state;
     const { isMobileView } = this.props;
+
     return (
       <div className="app-header-navigation-component">
         <nav className="navbar navbar-expand-md btco-hover-menu">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
-
-              <li className="nav-item">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link" href="#">test1 <span className="sr-only"></span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">test2</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">asdf</a>
-              </li>
-
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="https://bootstrapthemes.co" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item dropdown-toggle" href="#">Submenu</a>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#">Submenu action</a></li>
-                      <li><a className="dropdown-item" href="#">Another submenu action</a></li>
-
-
-                      <li><a className="dropdown-item dropdown-toggle" href="#">Subsubmenu</a>
-                        <ul className="dropdown-menu">
-                          <li><a className="dropdown-item" href="#">Subsubmenu action aa</a></li>
-                          <li><a className="dropdown-item" href="#">Another subsubmenu action</a></li>
-                        </ul>
-                      </li>
-                      <li><a className="dropdown-item dropdown-toggle" href="#">Second subsubmenu</a>
-                        <ul className="dropdown-menu">
-                          <li><a className="dropdown-item" href="#">Subsubmenu action bb</a></li>
-                          <li><a className="dropdown-item" href="#">Another subsubmenu action</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                  <li><a className="dropdown-item dropdown-toggle" href="#">Submenu 2</a>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#">Submenu action 2</a></li>
-                      <li><a className="dropdown-item" href="#">Another submenu action 2</a></li>
-
-
-                      <li><a className="dropdown-item dropdown-toggle" href="#">Subsubmenu</a>
-                        <ul className="dropdown-menu">
-                          <li><a className="dropdown-item" href="#">Subsubmenu action 1 3</a></li>
-                          <li><a className="dropdown-item" href="#">Another subsubmenu action 2 3</a></li>
-                        </ul>
-                      </li>
-                      <li><a className="dropdown-item dropdown-toggle" href="#">Second subsubmenu 3</a>
-                        <ul className="dropdown-menu">
-                          <li><a className="dropdown-item" href="#">Subsubmenu action 3 </a></li>
-                          <li><a className="dropdown-item" href="#">Another subsubmenu action 3</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
+              {this.renderCategories()}
             </ul>
           </div>
         </nav>
       </div>
-
     );
-    // return (
-    //   <div className={`app-header-navigation-component ${isMobileView ? 'mobile-view' : ''}`}>
-    //     <ul className="navbar-nav nav mr-auto mt-2 mt-lg-0">
-    //       {this.renderCategories()}
-    //     </ul>
-    //   </div>
-    // );
   }
 }
 
