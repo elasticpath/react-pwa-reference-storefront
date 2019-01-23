f35a9a94f1b1e6b395446c0435cd3dbb8ff35e41
Shaun Maharaj <Shaun.Maharaj@elasticpath.com>
2019-01-23 15:40:38 -0500

    Fetch configs from general source
    - Initial push of refactor

---
+++ src/utils/Config.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright © 2018 Elastic Path Software Inc. All rights reserved.
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this license. If not, see
+ *
+ *     https://www.gnu.org/licenses/
+ *
+ *
+ */
+
+const Config = require('Config');
+
+// TO-DO: Need to fall back to Config if configuration is not available in window.getConfig()
+export const config = process.env.NODE_ENV === 'development'
+  ? Config
+  : window.getConfig();
+
+export default config;
