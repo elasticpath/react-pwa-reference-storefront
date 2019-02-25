519fef1aacd2b5afd4b00d4e1f2352f9ecdba3c3
Petro Dubno <petro.dubno@gmail.com>
2019-02-25 11:43:54 +0200

    Added loginUser function

--- src/__tests__/e2e/profile.test.js
+++ src/__tests__/e2e/profile.test.js
@@ -63,9 +63,6 @@ const desktopViewport = {
 const LOGGED_IN_BUTTON = 'button[id="header_navbar_loggedIn_button"]';
 const DROPDOWN_LOGGED_IN_BUTTON = '.dropdown-toggle#header_navbar_loggedIn_button';
 const REGISTER_BUTTON = 'button[id="login_modal_register_button"]';
-const LOGIN_USERNAME_INPUT = 'input[id="login_modal_username_input"]';
-const LOGIN_PASSWORD_INPUT = 'input[id="login_modal_password_input"]';
-const LOGIN_BUTTON = 'button[id="login_modal_login_button"]';
 const MY_PROFILE = 'span[id="header_navbar_login_menu_profile_link"]';
 const PURCHASE_HISTORY = "div[data-region='profilePurchaseHistoryRegion']";
 const EDIT_PERSONAL_INFO_BUTTON = 'button.profile-personal-info-edit-btn';
@@ -98,9 +95,69 @@ const SAVE_TO_PROFILE = "#saveToProfile";
 const CONTINUE_BUTTON = "button.payment-save-btn";
 const ADDRESS_NAME = ".address-name";
 
-
 const EXPECTED_FIRST_NAME = 'john2';
 const EXPECTED_ADDRESS_NAME= 'Test User';
+
+async function loginUser(page) {
+  const userInfo = {
+    firstName: 'john2',
+    lastName: 'smith2',
+    username: `john2@ep.com`,
+    password: 'password',
+  };
+  const LOGIN_FEEDBACK = 'div[data-region="authLoginFormFeedbackRegion"]';
+  const REGISTER_BUTTON_CSS = "#login_modal_register_button";
+  const HOME_PAGE_CSS = 'div.home-page-component';
+
+  const LOGGED_IN_BUTTON = '#header_navbar_loggedIn_button';
+  const LOGIN_USERNAME_INPUT = '#login_modal_username_input';
+  const LOGIN_PASSWORD_INPUT = '#login_modal_password_input';
+  const LOGIN_BUTTON = '#login_modal_login_button';
+
+  const FORM_FIRST_NAME = '#registration_form_firstName';
+  const FORM_LAST_NAME = '#registration_form_lastName';
+  const FORM_EMAIL_USER_NAME = '#registration_form_emailUsername';
+  const FORM_PASSWORD = '#registration_form_password';
+  const FORM_CONFIRM_PASSWORD = '#registration_form_passwordConfirm';
+  const FORM_SUBMIT_BUTTON = '#registration_form_register_button';
+
+  await page.waitForSelector(LOGGED_IN_BUTTON);
+  await page.click(LOGGED_IN_BUTTON);
+
+  await page.waitForSelector(LOGIN_USERNAME_INPUT);
+  await page.type(LOGIN_USERNAME_INPUT, userInfo.username);
+
+  await page.waitForSelector(LOGIN_PASSWORD_INPUT);
+  await page.type(LOGIN_PASSWORD_INPUT, userInfo.password);
+
+  await page.waitForSelector(LOGIN_BUTTON);
+  await Promise.all([
+    page.click(LOGIN_BUTTON),
+  ]);
+  await page.waitFor(5000);
+
+  const feedbackElement = await page.$(LOGIN_FEEDBACK);
+  let feedbackText = '';
+  if (feedbackElement) {
+    feedbackText = await page.evaluate(el => el.textContent, feedbackElement);
+  }
+  if (feedbackText){
+    await page.waitForSelector(REGISTER_BUTTON_CSS);
+    await page.click(REGISTER_BUTTON_CSS);
+    await page.waitForSelector(FORM_FIRST_NAME);
+    await page.type(FORM_FIRST_NAME, userInfo.firstName);
+    await page.type(FORM_LAST_NAME, userInfo.lastName);
+    await page.type(FORM_EMAIL_USER_NAME, userInfo.username);
+    await page.type(FORM_PASSWORD, userInfo.password);
+    await page.type(FORM_CONFIRM_PASSWORD, userInfo.password);
+
+    await page.click(FORM_SUBMIT_BUTTON);
+    await page.waitForSelector(HOME_PAGE_CSS);
+  }
+  else {
+    await page.waitForSelector(HOME_PAGE_CSS);
+  }
+}
 
 describe('Profile', () => {
   test('Navigate Profile', async () => {
@@ -117,13 +174,7 @@ describe('Profile', () => {
     await page.goto(APP);
 
     // When I login as following registered shopper
-    await page.waitForSelector(LOGGED_IN_BUTTON);
-    await page.click(LOGGED_IN_BUTTON);
-    await page.waitForSelector(LOGIN_USERNAME_INPUT);
-    await page.$eval(LOGIN_USERNAME_INPUT, el => el.value = '');
-    await page.type(LOGIN_USERNAME_INPUT, userData.username);
-    await page.type(LOGIN_PASSWORD_INPUT, userData.password);
-    await page.click(LOGIN_BUTTON);
+    await loginUser(page);
 
     // When I navigate to the profile page
     await page.waitForSelector(DROPDOWN_LOGGED_IN_BUTTON);
@@ -133,7 +184,7 @@ describe('Profile', () => {
 
     // Then I can see my purchase history
     await page.waitForSelector(PURCHASE_HISTORY);
-  
+
     await browser.close();
   }, 20000);
 
@@ -151,12 +202,7 @@ describe('Profile', () => {
     await page.goto(APP);
 
     // When I login as following registered shopper
-    await page.waitForSelector(LOGGED_IN_BUTTON);
-    await page.click(LOGGED_IN_BUTTON);
-    await page.waitForSelector(LOGIN_USERNAME_INPUT);
-    await page.type(LOGIN_USERNAME_INPUT, userData.username);
-    await page.type(LOGIN_PASSWORD_INPUT, userData.password);
-    await page.click(LOGIN_BUTTON);
+    await loginUser(page);
 
     // When I navigate to the profile page
     await page.waitForSelector(DROPDOWN_LOGGED_IN_BUTTON);
@@ -183,7 +229,7 @@ describe('Profile', () => {
     const element = await page.$(CHECKING_FIRST_NAME);
     const text = await page.evaluate(el => el.textContent, element);
     expect(text).toEqual(EXPECTED_FIRST_NAME);
-  
+
     await browser.close();
   }, 30000);
 
@@ -200,7 +246,7 @@ describe('Profile', () => {
     });
     await page.goto(APP);
 
-    // I register a new user
+    // When I register a new user
     await page.waitForSelector(LOGGED_IN_BUTTON);
     await page.click(LOGGED_IN_BUTTON);
     await page.waitForSelector(REGISTER_BUTTON);
@@ -263,7 +309,7 @@ describe('Profile', () => {
     const element = await page.$(ADDRESS_NAME);
     const text = await page.evaluate(el => el.textContent, element);
     expect(text).toEqual(EXPECTED_ADDRESS_NAME);
-  
+
     await browser.close();
   }, 35000);
 });
