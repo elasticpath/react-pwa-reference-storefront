cf8ac830630a4571a9eeb4cce8657e28ebf3d984
Rostyk <rkanafotskyy@gmail.com>
2019-06-04 15:47:04 +0300

    Removed demo components

--- components/src/ComponentOne/ComponentOne.stories.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-
-import ComponentOne from './ComponentOne';
-
-storiesOf('ComponentOne', module)
-  .add('Default', () => <ComponentOne />);
