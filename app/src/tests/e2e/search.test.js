87c6c9e5998384d9a3023e7ef9324f821cf774d7
Shaun Maharaj <39800563+shaunmaharaj@users.noreply.github.com>
2019-06-10 14:11:29 -0400

    - Update port to 8080 (#317)

--- app/src/tests/e2e/search.test.js
+++ app/src/tests/e2e/search.test.js
@@ -28,7 +28,7 @@ const dataSearch = {
   searchInvalidKeyword: 'abc'
 };
 const host = process.env.TEST_HOST;
-const APP = host || 'http://localhost:3000/';
+const APP = host || 'http://localhost:8080/';
 const desktopViewport = {
   width: 1500,
   height: 700,
