1d9d70be067e623bf70cb711620107f88b6107e2
yulia-dnistrian <yulia.dnist@gmail.com>
2019-06-26 14:08:41 +0300

    fix AppHeaderNavigationMain story

--- components/src/AppHeaderNavigation/appheadernavigation.main.stories.tsx
+++ components/src/AppHeaderNavigation/appheadernavigation.main.stories.tsx
@@ -4,17 +4,11 @@ import { MemoryRouter } from 'react-router';
 
 import AppHeaderNavigationMain from './appheadernavigation.main';
 
-function handleIsOffline(isOfflineValue) {
-  this.setState({
-    isOffline: isOfflineValue,
-  });
-}
-
 storiesOf('AppHeaderNavigationMain', module)
   .add('AppHeaderNavigationMain', () => {
     return (
       <div style={{'backgroundColor':'#040060'}}>
-        <MemoryRouter initialEntries={['/']}><AppHeaderNavigationMain isMobileView={false} isOffline={false} isOfflineCheck={handleIsOffline} /></MemoryRouter>
+        <MemoryRouter initialEntries={['/']}><AppHeaderNavigationMain isMobileView={false} isOffline={false} isOfflineCheck={() => {}} /></MemoryRouter>
       </div>
     );
   });
