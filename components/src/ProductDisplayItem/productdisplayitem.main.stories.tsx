cb71104e600e667a2c03e4d000f877f64296b885
Alvin <alvin.chan@elasticpath.com>
2019-07-12 10:50:53 -0700

    Place dummy function into multi sku selector callback

--- components/src/ProductDisplayItem/productdisplayitem.main.stories.tsx
+++ components/src/ProductDisplayItem/productdisplayitem.main.stories.tsx
@@ -16,17 +16,23 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import ProductDisplayItemMain from './productdisplayitem.main';
 
+function dummyOnChangeProductFeature(path) {
+  // TODO: May want to include: https://github.com/gvaldambrini/storybook-router into project to show multi sku option transitions.
+  alert(`container passed in callback invoked with selected options sku: ${path}`);
+  window.location.reload();
+}
+
 storiesOf('ProductDisplayItemMain', module)
   .addDecorator(story => (
     <MemoryRouter initialEntries={['/itemdetail']}>{story()}</MemoryRouter>
   ))
   .add('ProductDisplayItemMain Plain', () =>  {
     mockProductDisplayItemMainPlain();
-    return <ProductDisplayItemMain productId='83992' />
+    return <ProductDisplayItemMain productId='83992'/>
   })
   .add('ProductDisplayItemMain Color/size', () =>  {
     mockProductDisplayItemMainColorAndSize();
-    return <ProductDisplayItemMain productId='VESTRI_WORDMARK_FITTED_HAT_RD' />
+    return <ProductDisplayItemMain productId='VESTRI_WORDMARK_FITTED_HAT_RD' onChangeProductFeature={dummyOnChangeProductFeature} />
   })
   .add('ProductDisplayItemMain Input', () =>  {
     mockProductDisplayItemMainInput();
