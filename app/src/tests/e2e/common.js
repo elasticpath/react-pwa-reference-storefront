373d1e69f35cda112ea91564736dfedc0c7aa86d
Shaun Maharaj <Shaun.Maharaj@elasticpath.com>
2019-07-13 16:33:58 -0400

    - attempt page navigation fix

--- app/src/tests/e2e/common.js
+++ app/src/tests/e2e/common.js
@@ -216,7 +216,7 @@ module.exports = {
 
       await Promise.all([
         page.click(FORM_SUBMIT_BUTTON),
-        page.waitForNavigation({ waitUntil: 'networkidle0' }),
+        page.waitForNavigation(),
       ]);
     }
   }
