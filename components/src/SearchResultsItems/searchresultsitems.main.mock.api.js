51c2059cc4afd45484309d289970a2c9d3ab6c19
Alvin <alvin.chan@elasticpath.com>
2019-07-15 16:19:42 -0700

    Mock searchresultsitems.main

---
+++ components/src/SearchResultsItems/searchresultsitems.main.mock.api.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright © 2018 Elastic Path Software Inc. All rights reserved.
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this license. If not, see
+ *
+ *     https://www.gnu.org/licenses/
+ *
+ *
+ */
+ import fetchMock from 'fetch-mock/es5/client';
+ import getSearchFromResponse from './MockHttpResponses/GET/getSearchForm_response.json';
+ import offerSearchResponse from './MockHttpResponses/POST/offerSearch_response.json';
+ 
+ function mockGetSearchForm(fetchMock) {
+     fetchMock.get(
+        '/cortex/?zoom=searches:keywordsearchform,searches:offersearchform',
+        getSearchFromResponse
+     );
+ }
+function mockOfferSearch(fetchMock) {
+    fetchMock.post(
+        /\/cortex\/offersearches\/[a-zA-Z0-9_]*\/offers\/form?(.*)/,
+        offerSearchResponse
+    );
+
+}
+ export function mockSearchResults() {
+     fetchMock.restore();
+     mockGetSearchForm(fetchMock);
+     mockOfferSearch(fetchMock);
+ }
