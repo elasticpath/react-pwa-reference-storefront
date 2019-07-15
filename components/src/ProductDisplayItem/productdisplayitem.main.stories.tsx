397e4873d8b79533bb87edb35ec4988067a282a4
aChanEP <Alvin.chan@elasticpath.com>
2019-07-15 12:22:48 -0700

    [RS-414 - PART 2] - Rs 452 mock productdisplayitem (#344)
    * Fix the address form to fetch properly
    
    * Fix and rename fetchAddressForm
    
    * rename json response files to the function names that make the http requests
    
    * rename json files and mock address submissions
    
    * Refactor the mock functions and create story for a failed submission
    
    * fix url that is inputted and be more explicit with http operations that are being mocked
    
    * get rid of unused comment
    
    * Update geography response
    
    * Update json response
    
    * update json response
    
    * Use regex to get rid of needing to match the scope
    
    * Be more specific about what the scope can be
    
    * Move addressData to local scope
    
    * Move over common http responses between components
    
    * add a common http response
    
    * Able to get the product display item main to show up
    
    * fix for address
    
    * Fix for geographies regex
    
    * add the color and size mock
    
    * mock the input product display
    
    * Able to mock the multi sku selection
    
    * Place dummy function into multi sku selector callback
    
    * Add all the multi sku option mocks
    
    * mock addToCart POST
    
    * rename function
    
    * Add new line to all new files
    
    * add new line chars
    
    * add another new line character
    
    * add new line character
    
    * FIx more linting errors

--- components/src/ProductDisplayItem/productdisplayitem.main.stories.tsx
+++ components/src/ProductDisplayItem/productdisplayitem.main.stories.tsx
@@ -5,16 +5,42 @@ import { MemoryRouter } from 'react-router';
 import '../style/reset.less';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/style.less';
+import { 
+  mockProductDisplayItemMainPlain, 
+  mockProductDisplayItemMainColorAndSize,
+  mockProductDisplayItemMainInput
+} from './productdisplayitem.main.api.mocks';
 
 // Import custom required scripts
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import ProductDisplayItemMain from './productdisplayitem.main';
 
+function dummyOnChangeProductFeature(path) {
+  // TODO: aChan - May want to include: https://github.com/gvaldambrini/storybook-router into project to show multi sku option transitions.
+  alert(`container passed in callback invoked with selected options sku: ${path}`);
+  window.location.reload();
+}
+
+function dummyHandleAddToCart() {
+  alert(`container passed addToCart callback invoked`);
+  window.location.reload();
+}
+
 storiesOf('ProductDisplayItemMain', module)
   .addDecorator(story => (
     <MemoryRouter initialEntries={['/itemdetail']}>{story()}</MemoryRouter>
   ))
-  .add('ProductDisplayItemMain Plain', () =>  <ProductDisplayItemMain productId='VESTRI_GOLF_BALLS' />)
-  .add('ProductDisplayItemMain Color/size', () =>  <ProductDisplayItemMain productId='VESTRI_WORDMARK_FITTED_HAT_RD' />)
-  .add('ProductDisplayItemMain Input', () =>  <ProductDisplayItemMain productId='250HR_PMKIT' />);
+  .add('ProductDisplayItemMain Plain', () =>  {
+    mockProductDisplayItemMainPlain();
+    
+    return <ProductDisplayItemMain productId='83992'onAddToCart={dummyHandleAddToCart} />
+  })
+  .add('ProductDisplayItemMain Color/size', () =>  {
+    mockProductDisplayItemMainColorAndSize();
+    return <ProductDisplayItemMain productId='VESTRI_WORDMARK_FITTED_HAT_RD' onChangeProductFeature={dummyOnChangeProductFeature} onAddToCart={dummyHandleAddToCart} />
+  })
+  .add('ProductDisplayItemMain Input', () =>  {
+    mockProductDisplayItemMainInput();
+    return <ProductDisplayItemMain productId='250HR_PMKIT' onAddToCart={dummyHandleAddToCart} />
+  });
