2bba5e3d8c765f76b6a0e5f95b0135f9cb47f41b
Alvin <alvin.chan@elasticpath.com>
2019-09-24 13:27:18 -0700

    Reorganize all bloomreach service types

--- components/src/BloomreachAppHeaderSearch/types/bloomreach.appheadersearch.main.d.ts
+++ components/src/BloomreachAppHeaderSearch/types/bloomreach.appheadersearch.main.d.ts
@@ -1,3 +1,5 @@
+export { BloomreachSearchSuggestionResponse } from '../../utils/Bloomreach/BloomreachSearchService'
+
 export interface BloomreachHeaderSearchMainProps {
     isMobileView: boolean,
     isFocused: boolean,
@@ -12,25 +14,4 @@ export interface BloomreachSuggestion {
 export interface BloomreachHeaderSearchMainState {
     keywords: string,
     suggestions: BloomreachSuggestion[],
-}
-
-export interface BloomreachSearchSuggestionResponse {
-  responseHeader: {
-    status: number;
-    QTime: number;
-  }
-  response: {
-    q: string;
-    suggestions: BloomreachSuggestion[];
-    numFound: number;
-    products: [{
-      url: string;
-      // eslint-disable-next-line
-      sale_price: number;
-      pid: string;
-      // eslint-disable-next-line
-      thumb_image: string;
-      title: string;
-    }];
-  };
 }
--- components/src/utils/Bloomreach/types/BloomreachSearchService.d.ts
+++ components/src/utils/Bloomreach/types/BloomreachSearchService.d.ts
@@ -70,3 +70,24 @@ interface Variant {
     sku_swatch_images: string[];
     sku_thumb_images:  string[];
 }
+
+export interface BloomreachSearchSuggestionResponse {
+    responseHeader: {
+      status: number;
+      QTime: number;
+    }
+    response: {
+      q: string;
+      suggestions: BloomreachSuggestion[];
+      numFound: number;
+      products: [{
+        url: string;
+        // eslint-disable-next-line
+        sale_price: number;
+        pid: string;
+        // eslint-disable-next-line
+        thumb_image: string;
+        title: string;
+      }];
+    };
+  }
