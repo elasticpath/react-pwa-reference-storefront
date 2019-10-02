e8b469893e890e8119b0d2592318111ed3c5e422
Alvin <alvin.chan@elasticpath.com>
2019-10-02 16:33:30 -0700

    adjust svg typings

--- app/custom_typings/image-loader.d.ts
+++ app/custom_typings/image-loader.d.ts
@@ -24,8 +24,10 @@ declare module '*.png' {
 }
 
 declare module '*.svg' {
-  const value: any;
-  export default value;
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
 
 declare module '*.jpg' {
--- storybook/custom_typings/image-loader.d.ts
+++ storybook/custom_typings/image-loader.d.ts
@@ -3,9 +3,11 @@ declare module "*.png" {
   export default value;
 }
 
-declare module "*.svg" {
-  const value: any;
-  export default value;
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
 
 declare module "*.jpg" {
