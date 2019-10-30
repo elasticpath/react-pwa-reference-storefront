7048c0615e694e67f1de3183e769822d9427f295
yulia-dnistrian <yulia.dnist@gmail.com>
2019-10-30 18:00:08 +0200

    add story for CartClear component

---
+++ components/src/CartClear/cartclear.stories.tsx
@@ -0,0 +1,36 @@
+/**
+ * Copyright © 2019 Elastic Path Software Inc. All rights reserved.
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this license. If not, see
+ *
+ *     https://www.gnu.org/licenses/
+ *
+ *
+ */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+import multiCartResponse from '../CommonMockHttpResponses/itemLookupMultiCart_response.json';
+
+import CartClear from './cartclear';
+
+storiesOf('CartClear', module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .add('CartClear', () => (
+    <div style={{ textAlign: 'center' }}>
+      <CartClear cartData={multiCartResponse._carts[0]._element[0]} handleCartsUpdate={() => {}} handleCartModalUpdate={() => {}} />
+    </div>
+  ));
