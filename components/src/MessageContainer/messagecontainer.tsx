d60e124d3857437eddff5930d7d19658d9aa6b45
yulia-dnistrian <yulia.dnist@gmail.com>
2019-11-05 17:15:51 +0200

    style debug messages

--- components/src/MessageContainer/messagecontainer.tsx
+++ components/src/MessageContainer/messagecontainer.tsx
@@ -20,32 +20,60 @@
  */
 
 import React from 'react';
-
+import { ReactComponent as CloseIcon } from '../../../app/src/images/icons/close-icon.svg';
+import { ReactComponent as ErrorIcon } from '../../../app/src/images/icons/error-icon.svg';
+import { ReactComponent as WarningIcon } from '../../../app/src/images/icons/warning-icon.svg';
+import { ReactComponent as InfoIcon } from '../../../app/src/images/icons/info-icon.svg';
 import './messagecontainer.less';
 
 interface MessageContainerProps {
   message: any
 }
 
-class MessageContainer extends React.Component<MessageContainerProps> {
-  componentDidMount() {}
+interface MessageContainerState {
+  showMessage: boolean,
+}
+
+class MessageContainer extends React.Component<MessageContainerProps, MessageContainerState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMessage: true,
+    };
+    this.handleCloseErrorMsg = this.handleCloseErrorMsg.bind(this);
+  }
+
+  componentWillReceiveProps() {
+    this.setState({ showMessage: true });
+  }
+
+  handleCloseErrorMsg() {
+    this.setState({ showMessage: false });
+  }
 
   render() {
+    const { showMessage } = this.state;
     const { message } = this.props;
-    if (message && message.debugMessages) {
+    if (message && message.debugMessages && showMessage) {
       let messageType = '';
       if (message.type === 'error' && message.id.includes('field')) {
         messageType = 'warning-message';
       } else if (message.type === 'error') {
         messageType = 'danger-message';
       } else if (message.type === 'needinfo') {
-        messageType = 'dark-message';
+        messageType = 'info-message';
       }
       return (
         <div className={`container debug-messages-container ${messageType}`}>
+          <ErrorIcon className="debug-msg-icon error" />
+          <WarningIcon className="debug-msg-icon warning" />
+          <InfoIcon className="debug-msg-icon info" />
           <p>
             {message.debugMessages}
           </p>
+          <button type="button" className="close-btn" onClick={this.handleCloseErrorMsg}>
+            <CloseIcon />
+          </button>
         </div>
       );
     }
