bd6ebe4f56e627c3c9e078649ac7550df1480afe
Rostyk <rkanafotskyy@gmail.com>
2019-11-12 17:00:55 +0200

    Added git creds

--- docker/components/components-publish.sh
+++ docker/components/components-publish.sh
@@ -15,11 +15,12 @@ if [[ ${isChanged} == ${publishDirName} ]]
 then
     git checkout RS-516-automate-version-increment
     # regex: (?s).*skip_ci.*
-    npm version patch  -m "Components %s skip_ci"
-    git push
-    # npm publish components/
-    echo "published components"
-
+    git config user.name 'CI'
+    git config user.email 'rkanafotskyy@gmail.com'
+    npm version patch -m "Components %s skip_ci"
+    git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/elasticpath/react-pwa-reference-storefront.git
+    # npm publish components || echo "Components publish failed"
     exit 0
 fi
-echo "components not published"
+echo "No changes in components folder"
+exit 0
