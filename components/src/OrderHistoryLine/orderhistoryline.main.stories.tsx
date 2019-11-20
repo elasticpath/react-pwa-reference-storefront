23cc3bf54c5bfe5f9c6018e7857f3780ba3e7466
Alvin <alvin.chan@elasticpath.com>
2019-11-20 15:39:58 -0800

    orderhistoryline.main.stories integrate with knobs

--- components/src/OrderHistoryLine/orderhistoryline.main.stories.tsx
+++ components/src/OrderHistoryLine/orderhistoryline.main.stories.tsx
@@ -21,11 +21,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { MemoryRouter } from 'react-router';
-
+import { text } from "@storybook/addon-knobs/react";
 import OrderHistoryLineMain from './orderhistoryline.main';
 
 storiesOf('OrderHistoryLineMain', module)
   .addDecorator(story => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
-  .add('OrderHistoryLineMain', () => <OrderHistoryLineMain orderHistoryLineUrlProps="" />);
+  .add('OrderHistoryLineMain', () => (<OrderHistoryLineMain orderHistoryLineUrlProps={text('orderHistoryLineUrlProps', '')} />));
