68ec1aebc13ee63dfbf56ff8d86919f4a58d24a4
Alvin <alvin.chan@elasticpath.com>
2019-11-20 15:59:30 -0800

    integrate knobs into paymentmethod.container

--- components/src/PaymentMethodContainer/paymentmethod.container.stories.tsx
+++ components/src/PaymentMethodContainer/paymentmethod.container.stories.tsx
@@ -21,10 +21,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import orderData from '../CommonMockHttpResponses/cart_data_response.json';
-
+import { object } from "@storybook/addon-knobs/react";
 import PaymentMethodContainer from './paymentmethod.container';
 
 const displayName = orderData._defaultcart[0]._order[0]._paymentmethodinfo[0]._paymentmethod[0];
 
 storiesOf('PaymentMethodContainer', module)
-  .add('PaymentMethodContainer', () => <PaymentMethodContainer displayName={displayName} />);
+  .add('PaymentMethodContainer', () => <PaymentMethodContainer displayName={object('displayName', displayName)} />);
