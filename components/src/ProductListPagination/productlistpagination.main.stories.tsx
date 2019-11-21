0e4ce09f9b53d2eed39f6220eb17f420a77077e6
Alvin <alvin.chan@elasticpath.com>
2019-11-21 06:00:08 -0800

    integrate knobs with productlistpagination

--- components/src/ProductListPagination/productlistpagination.main.stories.tsx
+++ components/src/ProductListPagination/productlistpagination.main.stories.tsx
@@ -22,6 +22,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { MemoryRouter } from 'react-router';
 import paginationDataProps from './MockHttpResponses/product_list_pagination_response.json';
+import { text, object, boolean } from "@storybook/addon-knobs/react";
 
 import ProductListPagination from './productlistpagination.main';
 
@@ -29,4 +30,13 @@ storiesOf('ProductListPagination', module)
   .addDecorator(story => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
-  .add('ProductListPagination', () => <ProductListPagination paginationDataProps={paginationDataProps} titleString="" isTop productListPaginationLinks={{}} />);
+  .add('ProductListPagination', () => {
+    return (
+      <ProductListPagination 
+        paginationDataProps={object('paginationDataProps', paginationDataProps)} 
+        titleString={text('titleString', '')} 
+        isTop={boolean('isTop', true)} 
+        productListPaginationLinks={object('productListPaginationLinks', {})}
+      />
+    );
+  });
