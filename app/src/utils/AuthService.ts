6c3f5dad1f85a95794e5292f68e8b45526f1b4d6
Matthew Kelly <matthew.kelly@elasticpath.com>
2019-12-12 14:56:57 -0500

    PB-7557 Logout support

--- app/src/utils/AuthService.ts
+++ app/src/utils/AuthService.ts
@@ -208,7 +208,27 @@ export function logout() {
 }
 
 export function logoutAccountManagementUser() {
-  logout().then(() => {
+  if (Config.b2b.openId.enable) {
+    return new Promise(((resolve, reject) => {
+      adminFetch('/oauth2/tokens', {
+        method: 'delete',
+        Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthTokenAuthService`),
+      })
+        .then((res) => {
+          logout();
+          resolve(res);
+        })
+        .then(() => {
+          window.location.href = '/';
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.error(error.message);
+          reject(error);
+        });
+    }));
+  }
+  return logout().then(() => {
     const keycloakLogoutRedirectUrl = `${Config.b2b.keycloak.logoutRedirectUrl}?redirect_uri=${encodeURIComponent(Config.b2b.keycloak.callbackUrl)}`;
     window.location.href = keycloakLogoutRedirectUrl;
   });
