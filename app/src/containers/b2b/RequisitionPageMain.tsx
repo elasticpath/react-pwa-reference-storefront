c3b0787fb1c79207a9744e147a60d4d8d0dd3e9e
Petro Dubno <petro.dubno@gmail.com>
2019-12-26 15:11:15 +0200

    Added RequisitionPageMain

--- app/src/containers/b2b/RequisitionPageMain.tsx
+++ app/src/containers/b2b/RequisitionPageMain.tsx
@@ -41,7 +41,11 @@ class RequisitionPageMain extends Component<RequisitionPageMainProps, Requisitio
 
   render() {
     const { isLoading } = this.state;
-
+    const products = {
+      list: [
+        { name: 'January 2020' }, { name: 'February 2020' }, { name: 'March 2020' }, { name: 'April 2020' }, { name: 'May 2020' }, { name: 'June 2020' }, { name: 'July 2020' }, { name: 'August 2020' }, { name: 'September 2020' }, { name: 'October 2020' }, { name: 'November 2020' }, { name: 'December 2020' },
+      ],
+    };
     return (
       <div className="requisition-component">
         {isLoading ? (
@@ -51,15 +55,38 @@ class RequisitionPageMain extends Component<RequisitionPageMainProps, Requisitio
             <div className="requisition-header">
               <Link className="back-link" to="/b2b">
                 <div className="back-arrow" />
-                {intl.get('back')}
+                {intl.get('back-to-lists')}
               </Link>
               <div className="name-container">
                 <Link className="back-link-mobile" to="/b2b">
                   <div className="back-arrow" />
-                  {intl.get('back')}
+                  {intl.get('back-to-lists')}
                 </Link>
                 <div className="name">
                   Vancouver
+                </div>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <Link to="#" href="/" className="edit-name">
+                  Edit
+                </Link>
+              </div>
+            </div>
+            <div className="header-container">
+              <button type="button" className="ep-btn primary wide add-to-list-button">Add product to list</button>
+              <div className="add-to-cart-dropdown">
+                <div className="dropdown-sort-field">
+                  <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      chosenSelect
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      {(products.list[0]) ? products.list.map(sortChoice => (
+                        <button type="button" className="dropdown-item" key={sortChoice.name} id={`product_display_item_sku_option_${sortChoice.name}`} value={sortChoice.name}>
+                          {sortChoice.name}
+                        </button>
+                      )) : ''}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
