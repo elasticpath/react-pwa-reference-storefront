81c194185a0ec0e6cf07a7cf4502186c887b198d
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-01-09 00:51:14 -0800

    Updated docker files

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sed -i.bak "s@\(\"pathForProxy\": \"\)[^\"]*\"@\1${CORTEX}\"@" ./app/src/ep.config.json
-sed -i.bak "s@\(\"scope\": \"\)[^\"]*\"@\1${STORE}\"@" ./app/src/ep.config.json
+sed -i.bak "s@\(\"pathForProxy\": \"\)[^\"]*\"@\1${CORTEX}\"@" ./src/ep.config.json
+sed -i.bak "s@\(\"scope\": \"\)[^\"]*\"@\1${STORE}\"@" ./src/ep.config.json
 
 exec "$@"
