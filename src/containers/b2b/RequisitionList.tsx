793d4910690ccc8b653a2184c7a5ad34b443f5c4
Rostyk <rkanafotskyy@gmail.com>
2020-01-28 17:40:07 +0200

    Updated requisition list table

--- src/containers/b2b/RequisitionList.tsx
+++ src/containers/b2b/RequisitionList.tsx
@@ -23,21 +23,42 @@ import React, { Component } from 'react';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import Modal from 'react-responsive-modal';
 import intl from 'react-intl-universal';
-
-import './RequisitionList.less';
 import { Link } from 'react-router-dom';
+
 import { ReactComponent as AddToCartIcon } from '../../images/icons/ic_add_to_cart.svg';
 import { ReactComponent as RecycleBinIcon } from '../../images/icons/ic_trash.svg';
+import * as Config from '../../ep.config.json';
+import { login } from '../../utils/AuthService';
+import { cortexFetch } from '../../utils/Cortex';
+
+import './RequisitionList.less';
+
+const listsZoomArray = [
+  'itemlistinfo',
+  'itemlistinfo:allitemlists',
+  'itemlistinfo:itemlisttypes',
+  'itemlistinfo:allitemlists:createitemlistform',
+  'itemlistinfo:allitemlists:element',
+  'itemlistinfo:allitemlists:element:additemlisttocartforms',
+  'itemlistinfo:allitemlists:element:additemlisttocartforms:element',
+  'itemlistinfo:allitemlists:element:additemlisttocartforms:element:target',
+  'itemlistinfo:allitemlists:element:additemlisttocartforms:element:target:descriptor',
+  'itemlistinfo:allitemlists:element:additemlisttocartforms:element:additemlisttocartaction',
+  'itemlistinfo:allitemlists:element:itemlists',
+  'itemlistinfo:allitemlists:element:lineitems',
+  'itemlistinfo:allitemlists:element:itemlisttype',
+];
 
 interface CartCreateProps {
   history: any
 }
 interface CartCreateState {
+  createRequisitionForm: any,
   requisitionElements: any,
-  multiCartData: any,
   openModal: boolean,
   listName: string,
   listNameErrorMessages: string,
+  isLoading: boolean,
 }
 
 class RequisitionList extends Component<CartCreateProps, CartCreateState> {
@@ -51,48 +72,13 @@ class RequisitionList extends Component<CartCreateProps, CartCreateState> {
   constructor(props) {
     super(props);
     this.state = {
-      requisitionElements: [
-        {
-          name: 'Vancouver',
-          'product-count': 16,
-        },
-        {
-          name: 'Chicago',
-          'product-count': 27,
-        },
-        {
-          name: 'New York',
-          'product-count': 56,
-        },
-        {
-          name: 'San Francisco',
-          'product-count': 89,
-        },
-        {
-          name: 'Toronto',
-          'product-count': 14,
-        },
-      ],
-      multiCartData: [
-        {
-          name: 'Default',
-        },
-        {
-          name: 'Office',
-        },
-        {
-          name: 'Showroom',
-        },
-        {
-          name: 'Kitchen',
-        },
-        {
-          name: 'Garage',
-        },
-      ],
+      // eslint-disable-next-line react/no-unused-state
+      createRequisitionForm: '',
+      requisitionElements: [],
       openModal: false,
       listName: '',
       listNameErrorMessages: '',
+      isLoading: false,
     };
     this.handleEditRequisition = this.handleEditRequisition.bind(this);
     this.handleDeleteRequisition = this.handleDeleteRequisition.bind(this);
@@ -103,15 +89,53 @@ class RequisitionList extends Component<CartCreateProps, CartCreateState> {
     this.handleChange = this.handleChange.bind(this);
     this.clearListNameField = this.clearListNameField.bind(this);
     this.handleSaveList = this.handleSaveList.bind(this);
+    this.addToSelectedCart = this.addToSelectedCart.bind(this);
   }
 
   componentDidMount() {
+    this.loadRequisitionListData();
   }
 
-  handleDeleteRequisition(element, index) {
-    const { requisitionElements } = this.state;
-    // eslint-disable-next-line no-console
-    console.log('Delete', element, index);
+  loadRequisitionListData() {
+    this.setState({ isLoading: true });
+    login()
+      .then(() => {
+        cortexFetch(`?zoom=${listsZoomArray.sort().join()}`, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
+          },
+        })
+          .then(res => res.json())
+          .then((res) => {
+            this.setState({
+              isLoading: false,
+              requisitionElements: res._itemlistinfo[0]._allitemlists[0]._element,
+              createRequisitionForm: res._itemlistinfo[0]._allitemlists[0]._createitemlistform[0],
+            });
+          });
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error.message);
+      });
+  }
+
+  handleDeleteRequisition(element) {
+    cortexFetch(element.self.uri, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
+      },
+    })
+      .then(() => {
+        this.loadRequisitionListData();
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error.message);
+      });
   }
 
   handleModalOpen() {
@@ -148,12 +172,31 @@ class RequisitionList extends Component<CartCreateProps, CartCreateState> {
   }
 
   handleSaveList() {
-    const { listName } = this.state;
+    const { listName, createRequisitionForm } = this.state;
     if (listName.length === 0) {
       this.setState({ listNameErrorMessages: intl.get('name-is-required') });
     } else {
-      this.setState({ listNameErrorMessages: '' });
-      this.handleModalClose();
+      cortexFetch(createRequisitionForm.self.uri, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
+        },
+        body: JSON.stringify({
+          name: listName,
+        }),
+      })
+        .then((res) => {
+          if (res.status === 200 || res.status === 201) {
+            this.setState({ listNameErrorMessages: '' });
+            this.loadRequisitionListData();
+            this.handleModalClose();
+          }
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.error(error.message);
+        });
     }
   }
 
@@ -170,7 +213,7 @@ class RequisitionList extends Component<CartCreateProps, CartCreateState> {
         </p>
         <div className="btn-wrap">
           <button type="button" className="ep-btn cancel-btn" onClick={() => this.handleCancelEditRequisition(index)}>{intl.get('cancel')}</button>
-          <button type="button" className="ep-btn ok-btn" onClick={event => this.handleDeleteRequisition(element, index)}>
+          <button type="button" className="ep-btn ok-btn" onClick={() => this.handleDeleteRequisition(element)}>
             {element.removeCartLoading ? (
               <div className="miniLoader" />
             ) : (
@@ -184,19 +227,40 @@ class RequisitionList extends Component<CartCreateProps, CartCreateState> {
     );
   }
 
-  dropdownCartSelection() {
-    const { multiCartData } = this.state;
+  addToSelectedCart(cart) {
+    cortexFetch(cart.self.uri, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
+      },
+      body: JSON.stringify({}),
+    })
+      .then(() => {
+        this.loadRequisitionListData();
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error.message);
+      });
+  }
+
+  dropdownCartSelection(list) {
+    const multiCartData = list._additemlisttocartforms[0]._element;
     return (
       <div className="cart-selection-menu">
         <h6 className="dropdown-header">
           {intl.get('add-to-cart')}
         </h6>
         <div className="cart-selection-menu-wrap">
-          {multiCartData.map(cart => (
-            <div className="dropdown-item cart-selection-menu-item" key={cart.name}>
-              {cart.name}
-            </div>
-          ))}
+          {multiCartData.map((cart) => {
+            const name = cart._target[0]._descriptor[0].name || intl.get('default');
+            return (
+              <button type="button" className="dropdown-item cart-selection-menu-item" key={name} onClick={() => this.addToSelectedCart(cart)}>
+                {name}
+              </button>
+            );
+          })}
         </div>
       </div>
     );
@@ -209,7 +273,7 @@ class RequisitionList extends Component<CartCreateProps, CartCreateState> {
         <li className={`requisition-list-item ${el.deleteMode ? 'edit-mode-state' : ''}`} key={`requisitionItem_${el.name ? el.name.trim() : 'default'}`} role="presentation">
           <Link className="requisition-info requisition-list-name" to="/b2b/requisition-list-item">{el.name}</Link>
           <p className="requisition-info product-count">
-            {el['product-count']}
+            {el['item-count']}
           </p>
           <div className="requisition-info action-btn">
             <div className="requisition-editing-btn">
@@ -218,7 +282,7 @@ class RequisitionList extends Component<CartCreateProps, CartCreateState> {
                   <AddToCartIcon className="add-to-cart-icon" />
                 </button>
                 <div className="dropdown-menu cart-selection-list">
-                  {this.dropdownCartSelection()}
+                  {this.dropdownCartSelection(el)}
                 </div>
               </div>
               <button className="ep-btn delete-btn" type="button" onClick={event => this.handleEditRequisition(event, index)}>
@@ -234,50 +298,58 @@ class RequisitionList extends Component<CartCreateProps, CartCreateState> {
   }
 
   render() {
-    const { listName, openModal, listNameErrorMessages } = this.state;
+    const {
+      listName, openModal, listNameErrorMessages, isLoading,
+    } = this.state;
 
     return (
       <div>
-        <div className="create-requisition-list">
-          <p>{intl.get('requisition-lists-description')}</p>
-          <button type="button" className="ep-btn primary create-list-btn" onClick={this.handleModalOpen}>{intl.get('create-list')}</button>
-          <Modal open={openModal} onClose={this.handleModalClose}>
-            <div className="modal-lg create-list-modal">
-              <div className="dialog-header">
-                <h2 className="modal-title">
-                  {intl.get('create-list')}
-                </h2>
-              </div>
-              <div className="dialog-content">
-                <div className="create-list-form">
-                  <div className="create-list-form-wrap">
-                    <label htmlFor="list_name">{intl.get('name')}</label>
-                    <input type="text" className={`list-name ${(listNameErrorMessages !== '') ? 'input-code-error' : ''}`} id="list_name" value={listName} onChange={this.handleChange} />
-                    {listName.length > 0 && (<span role="presentation" className="clear-field-btn" onClick={this.clearListNameField} />)}
-                    <span className={`${(listNameErrorMessages !== '') ? 'input-error-icon' : ''}`} />
-                    <p className="error-message">{listNameErrorMessages}</p>
+        {isLoading ? (
+          <div className="loader" />
+        ) : (
+          <div>
+            <div className="create-requisition-list">
+              <p>{intl.get('requisition-lists-description')}</p>
+              <button type="button" className="ep-btn primary create-list-btn" onClick={this.handleModalOpen}>{intl.get('create-list')}</button>
+              <Modal open={openModal} onClose={this.handleModalClose}>
+                <div className="modal-lg create-list-modal">
+                  <div className="dialog-header">
+                    <h2 className="modal-title">
+                      {intl.get('create-list')}
+                    </h2>
+                  </div>
+                  <div className="dialog-content">
+                    <div className="create-list-form">
+                      <div className="create-list-form-wrap">
+                        <label htmlFor="list_name">{intl.get('name')}</label>
+                        <input type="text" className={`list-name ${(listNameErrorMessages !== '') ? 'input-code-error' : ''}`} id="list_name" value={listName} onChange={this.handleChange} />
+                        {listName.length > 0 && (<span role="presentation" className="clear-field-btn" onClick={this.clearListNameField} />)}
+                        <span className={`${(listNameErrorMessages !== '') ? 'input-error-icon' : ''}`} />
+                        <p className="error-message">{listNameErrorMessages}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="dialog-footer">
+                    <button className="cancel" type="button" onClick={this.handleModalClose}>{intl.get('cancel')}</button>
+                    <button className="upload" type="button" onClick={this.handleSaveList}>
+                      {intl.get('save')}
+                    </button>
                   </div>
                 </div>
-              </div>
-              <div className="dialog-footer">
-                <button className="cancel" type="button" onClick={this.handleModalClose}>{intl.get('cancel')}</button>
-                <button className="upload" type="button" onClick={this.handleSaveList}>
-                  {intl.get('save')}
-                </button>
-              </div>
+              </Modal>
             </div>
-          </Modal>
-        </div>
-        <div className="requisition-list-wrap">
-          <ul className="requisition-list">
-            <li className="requisition-list-item requisition-list-header">
-              <h4 className="requisition-info">{intl.get('name')}</h4>
-              <h4 className="requisition-info">{intl.get('product-count')}</h4>
-              <h4 className="requisition-info action-btn">{intl.get('actions')}</h4>
-            </li>
-            {this.renderRequisitionItems()}
-          </ul>
-        </div>
+            <div className="requisition-list-wrap">
+              <ul className="requisition-list">
+                <li className="requisition-list-item requisition-list-header">
+                  <h4 className="requisition-info">{intl.get('name')}</h4>
+                  <h4 className="requisition-info">{intl.get('product-count')}</h4>
+                  <h4 className="requisition-info action-btn">{intl.get('actions')}</h4>
+                </li>
+                {this.renderRequisitionItems()}
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
