4a3a301748a0c0954a24c07888cd703ddee3ad3a
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-02-10 23:25:43 -0800

    Added Netlify build script

---
+++ netlifybuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat src/ep.config.json | jq \
+  --arg SCOPE "$CORTEX_SCOPE" \
+  --arg PROXY "$CORTEX_PATH" \
+  '.cortexApi.scope = $SCOPE | .cortexApi.pathForProxy = $PROXY' \
+  > src/ep.config.json.temp
+
+mv src/ep.config.json.temp src/ep.config.json
+
+yarn build
+
+cp src/ep.config.json build/ep.config.json
