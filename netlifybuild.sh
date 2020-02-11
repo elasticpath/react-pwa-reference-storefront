dd3bc946f92a1ec1344815f445713d918495dba1
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-02-11 00:01:33 -0800

    Using path only

--- netlifybuild.sh
+++ netlifybuild.sh
@@ -2,8 +2,8 @@
 
 cat src/ep.config.json | jq \
   --arg SCOPE "$CORTEX_SCOPE" \
-  --arg PROXY "$CORTEX_PATH" \
-  '.cortexApi.scope = $SCOPE | .cortexApi.pathForProxy = $PROXY' \
+  --arg PATH "$CORTEX_PATH" \
+  '.cortexApi.scope = $SCOPE | .cortexApi.path = $PATH' \
   > src/ep.config.json.temp
 
 mv src/ep.config.json.temp src/ep.config.json
