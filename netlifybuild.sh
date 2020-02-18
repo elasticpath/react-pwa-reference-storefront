39f6be254b57b63e11391bf88dbabd7218b57f35
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-02-17 23:57:28 -0800

    Removed testing

--- netlifybuild.sh
+++ netlifybuild.sh
@@ -5,20 +5,20 @@ set -e
 
 export CI=true
 
-cat src/ep.config.json | jq \
-  --arg SCOPE "$CORTEX_SCOPE" \
-  --arg PATH "$CORTEX_PATH" \
-  '.cortexApi.scope = $SCOPE | .cortexApi.path = $PATH' \
-  > src/ep.config.json.temp
+# cat src/ep.config.json | jq \
+#   --arg SCOPE "$CORTEX_SCOPE" \
+#   --arg PATH "$CORTEX_PATH" \
+#   '.cortexApi.scope = $SCOPE | .cortexApi.path = $PATH' \
+#   > src/ep.config.json.temp
 
-mv src/ep.config.json.temp src/ep.config.json
+# mv src/ep.config.json.temp src/ep.config.json
 
 yarn build
 
-cp ./_redirects build/
+# cp ./_redirects build/
 
-./node_modules/.bin/http-server build/ -p 8080 &>/dev/null &
+# ./node_modules/.bin/http-server build/ -p 8080 &>/dev/null &
 
-yarn test
+# yarn test
 
-kill $(jobs -p)
+# kill $(jobs -p)
