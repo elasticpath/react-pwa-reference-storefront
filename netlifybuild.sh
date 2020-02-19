b486ae1a5a707081511fd5977277456373107809
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-02-18 22:31:59 -0800

    Enabled testing back

--- netlifybuild.sh
+++ netlifybuild.sh
@@ -5,20 +5,12 @@ set -e
 
 export CI=true
 
-# cat src/ep.config.json | jq \
-#   --arg SCOPE "$CORTEX_SCOPE" \
-#   --arg PATH "$CORTEX_PATH" \
-#   '.cortexApi.scope = $SCOPE | .cortexApi.path = $PATH' \
-#   > src/ep.config.json.temp
+yarn build
 
-# mv src/ep.config.json.temp src/ep.config.json
+cp ./_redirects build/
 
- yarn build
+./node_modules/.bin/http-server build/ -p 8080 &>/dev/null &
 
-# cp ./_redirects build/
+yarn test
 
-# ./node_modules/.bin/http-server build/ -p 8080 &>/dev/null &
-
-# yarn test
-
-# kill $(jobs -p)
+kill $(jobs -p)
