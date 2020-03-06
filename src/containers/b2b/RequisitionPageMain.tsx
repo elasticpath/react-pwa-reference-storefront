a8bb5299dbc4152b8f9f5c4f81b196595e9ca130
yulia-dnistrian <yulia.dnist@gmail.com>
2020-03-06 16:03:41 +0200

    implement select all items in req list

--- src/containers/b2b/RequisitionPageMain.tsx
+++ src/containers/b2b/RequisitionPageMain.tsx
@@ -33,7 +33,6 @@ import { ReactComponent as CloseIcon } from '../../images/icons/ic_close.svg';
 import { login } from '../../utils/AuthService';
 import { cortexFetch } from '../../utils/Cortex';
 import * as Config from '../../ep.config.json';
-import { useCountDispatch } from '../../components/src/cart-count-context';
 
 import './RequisitionPageMain.less';
 import Pagination from '../../components/src/Pagination/pagination';
@@ -70,6 +69,7 @@ const listsZoomArray = [
   'paginatedlineitems:element:item:wishlistmemberships',
   'paginatedlineitems:next',
   'paginatedlineitems:previous',
+  'paginatedlineitems:paginator',
 ];
 
 const elementZoomArray = [
@@ -110,6 +110,7 @@ interface RequisitionPageMainState {
   selectedProducts: any,
   addItemsToItemListUri: string,
   removeLineItemsUri: string,
+  paginatorUri: string,
   showCreateListLoader: boolean,
   addToCartLoader: boolean,
   listItemCount: number,
@@ -134,6 +135,7 @@ class RequisitionPageMain extends Component<RouteComponentProps<RequisitionPageM
       selectedProducts: [],
       addItemsToItemListUri: '',
       removeLineItemsUri: '',
+      paginatorUri: '',
       showCreateListLoader: false,
       addToCartLoader: false,
       listItemCount: 0,
@@ -202,6 +204,7 @@ class RequisitionPageMain extends Component<RouteComponentProps<RequisitionPageM
                 multiCartData: res._additemlisttocartforms[0]._element,
                 currentlyListName: res.name,
                 productsData: res._paginatedlineitems[0],
+                paginatorUri: res._paginatedlineitems[0]._paginator[0].self.uri,
               });
               this.handleUpdateSelectedItem();
             }
@@ -246,7 +249,7 @@ class RequisitionPageMain extends Component<RouteComponentProps<RequisitionPageM
     }
     this.setState({ selectedProducts: products });
 
-    if (!isChecked && (productsData._next || productsData._previous)) {
+    if (!isChecked && (productsData._next || productsData._previous) && products.length !== productsData.pagination.results) {
       this.setState({ showSelectAllPopup: true });
     } else {
       this.setState({ showSelectAllPopup: false });
@@ -441,31 +444,35 @@ class RequisitionPageMain extends Component<RouteComponentProps<RequisitionPageM
   }
 
   handleSelectAllItems() {
-    const { productsData } = this.state;
-
-    const { match } = this.props;
-    const listUri = match.params.uri;
-    const scope = localStorage.getItem(`${Config.cortexApi.scope}_oAuthScope`);
-
-    const { pages } = productsData.pagination;
-    const pagesArr = Array.from(new Array(pages), (val, index) => index + 1);
-
+    const { productsData, paginatorUri } = this.state;
+    const size = productsData.pagination.results;
     this.setState({ selectAllLoader: true });
-    const promises = pagesArr.map(pageIndex => cortexFetch(`/itemlists/${scope}/${listUri}/lineitems/pages/${pageIndex}?zoom=${elementZoomArray.sort().join()}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
-      },
-    }).then(res => res.json()));
-    Promise.all(promises)
-      .then((res:any) => {
-        const products = res.filter(el => el._element).map(el => el._element).flat();
-        this.setState({ selectedProducts: products, selectAllLoader: false, showSelectAllPopup: false });
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error(error.message);
-      });
+
+    login().then(() => {
+      cortexFetch(`${paginatorUri}?zoom=${elementZoomArray.sort().join()}&followlocation&format=standardlinks,zoom.nodatalinks`,
+        {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: localStorage.getItem(`${Config.cortexApi.scope}_oAuthToken`),
+          },
+          body: JSON.stringify({ size }),
+        })
+        .then(res => res.json())
+        .then((res) => {
+          this.setState({
+            showSelectAllPopup: false,
+            selectedProducts: res._element,
+            selectAllLoader: false,
+          });
+          // this.loadRequisitionListData(true);
+        })
+        .catch((error) => {
+          this.setState({ selectAllLoader: false });
+          // eslint-disable-next-line no-console
+          console.error('error.message:', error.message);
+        });
+    });
   }
 
   handleCloseSelectAllPopup() {
@@ -515,22 +522,22 @@ class RequisitionPageMain extends Component<RouteComponentProps<RequisitionPageM
 
     return (
       <div className="requisition-component">
-        {/* {showSelectAllPopup && (
-         <div className="select-all-items-block">
-         <div className={`select-all-items ${selectAllLoader ? 'loading' : ''}`}>
+        {showSelectAllPopup && (
+        <div className="select-all-items-block">
+          <div className={`select-all-items ${selectAllLoader ? 'loading' : ''}`}>
             {selectAllLoader ? (<span className="miniLoader" />) : ''}
-             <p>
+            <p>
               {msg.map((str) => {
-                 const i = Object.keys(obj).indexOf(str);
+                const i = Object.keys(obj).indexOf(str);
                 return i === -1 ? str : obj[str];
-               })}
-             </p>
-             <button type="button" className="ep-btn small close-btn" onClick={this.handleCloseSelectAllPopup}>
-               {intl.get('close')}
-             </button>
-            </div>
+              })}
+            </p>
+            <button type="button" className="ep-btn small close-btn" onClick={this.handleCloseSelectAllPopup}>
+              {intl.get('close')}
+            </button>
           </div>
-         )} */}
+        </div>
+        )}
         {isLoading ? (
           <div className="loader" />
         ) : (
