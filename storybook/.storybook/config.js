7fe825baff282f1039c86cfe1b5545b2a17341e3
Shaun Maharaj <Shaun.Maharaj@elasticpath.com>
2020-03-30 16:59:08 -0400

    Fix for Storybook running in dark mode
    - Fix for Storybook running in dark mode

--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -4,6 +4,7 @@ import epConfig from '../../src/ep.config';
 import intl from 'react-intl-universal';
 import { addParameters } from '@storybook/react';
 import { addDecorator } from '@storybook/react';
+import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
 import { addReadme } from 'storybook-readme';
 import elasticpath from './elasticpath';
@@ -16,6 +17,10 @@ addParameters({
   options: {
     theme: elasticpath,
   },
+});
+
+addons.setConfig({
+  theme: themes.light,
 });
 
 const locales = {};
