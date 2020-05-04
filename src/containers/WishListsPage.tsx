ced045473f30dc01f9ff5a3c66183fec172865cd
Alvin <alvin.chan@elasticpath.com>
2020-05-04 11:06:27 -0400

    import wishlist

--- src/containers/WishListsPage.tsx
+++ src/containers/WishListsPage.tsx
@@ -25,7 +25,7 @@ import { RouteComponentProps } from 'react-router-dom';
 import { login } from '../utils/AuthService';
 import { cortexFetch } from '../utils/Cortex';
 import Config from '../ep.config.json';
-
+import WishListMain from '../components/src/WishList/wishlist.main';
 import './WishListsPage.scss';
 
 // Array of zoom parameters to pass to Cortex
